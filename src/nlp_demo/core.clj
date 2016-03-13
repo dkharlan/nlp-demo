aa28e097e8a514a81355987dffd5a0e4ee10f6a0
Dave Harlan <david.k.harlan@gmail.com>
2016-03-13 16:35:26 -0400

    Added some functions for extracting basic information about the sentences of a document, including part-of-speech tags and named entity recognition

--- src/nlp_demo/core.clj
+++ src/nlp_demo/core.clj
@@ -7,17 +7,31 @@
            (org.apache.pdfbox.util PDFTextStripper)
            (edu.stanford.nlp.pipeline StanfordCoreNLP Annotation)
            (java.util Properties)
-           (edu.stanford.nlp.ling CoreAnnotations$TokensAnnotation)))
+           (edu.stanford.nlp.ling CoreAnnotations$TokensAnnotation CoreAnnotations$SentencesAnnotation CoreAnnotations$PartOfSpeechAnnotation CoreAnnotations$LemmaAnnotation CoreAnnotations$NamedEntityTagAnnotation CoreAnnotations$NormalizedNamedEntityTagAnnotation CoreAnnotations$TextAnnotation)
+           (edu.stanford.nlp.semgraph SemanticGraphCoreAnnotations$BasicDependenciesAnnotation SemanticGraphCoreAnnotations$CollapsedDependenciesAnnotation SemanticGraphCoreAnnotations$CollapsedCCProcessedDependenciesAnnotation)
+           (edu.stanford.nlp.dcoref CorefCoreAnnotations$CorefChainAnnotation)))
 
-(def default-annotator-props (props/map->properties {:annotators "tokenize"}))
+(def annotators {:tokenize CoreAnnotations$TokensAnnotation
+                 :ssplit   CoreAnnotations$SentencesAnnotation
+                 :pos      CoreAnnotations$PartOfSpeechAnnotation
+                 :lemma    CoreAnnotations$LemmaAnnotation
+                 :ner      [CoreAnnotations$NamedEntityTagAnnotation
+                            CoreAnnotations$NormalizedNamedEntityTagAnnotation]
+                 :parse    [SemanticGraphCoreAnnotations$BasicDependenciesAnnotation
+                            SemanticGraphCoreAnnotations$CollapsedDependenciesAnnotation
+                            SemanticGraphCoreAnnotations$CollapsedCCProcessedDependenciesAnnotation]
+                 :dcoref   CorefCoreAnnotations$CorefChainAnnotation})
+
+; tokenize, pos and parse added as dependencies for ssplit, lemma and dcoref respectively
+(def default-annotator-props
+  (props/map->properties {:annotators (apply str (interpose ", " (map name (keys annotators))))}))
 
 (defn read-pdf-text [file]
   (let [parser (PDFParser. (io/input-stream file))]
     (.parse parser)
     (with-open [cos-doc (.getDocument parser)
                 pd-doc (PDDocument. cos-doc)]
-      (let [stripper (PDFTextStripper.)]
-        (.getText stripper pd-doc)))))
+      (.getText (PDFTextStripper.) pd-doc))))
 
 (defn read-all-pdfs [resource-dir]
   (map read-pdf-text (-> resource-dir
@@ -31,6 +45,28 @@
         annotation (Annotation. ^String text)]
     (.annotate pipeline annotation)
     annotation))
+
+(defn print-token-info [token]
+  (let [text (.get token CoreAnnotations$TextAnnotation)
+        part-of-speech (.get token CoreAnnotations$PartOfSpeechAnnotation)
+        named-entity (.get token CoreAnnotations$NamedEntityTagAnnotation)
+        normalized-named-entity (.get token CoreAnnotations$NormalizedNamedEntityTagAnnotation)]
+    (printf "%-16s%-8s%-15s%-5s\n" text part-of-speech named-entity (or normalized-named-entity ""))))
+
+(defn print-sentence-info [sentence]
+  (let [text (.get sentence CoreAnnotations$TextAnnotation)
+        tokens (.get sentence CoreAnnotations$TokensAnnotation)
+        dependency-graph (.get sentence SemanticGraphCoreAnnotations$CollapsedCCProcessedDependenciesAnnotation)]
+    (printf "Sentence: %s\n" text)
+    (printf "%-16s%-8s%-15s%-5s" "Word" "POS" "Ent." "N.Ent.\n")
+    (println "-------------------------------------------------")
+    (doseq [token tokens]
+      (print-token-info token))
+    (printf "Dependency graph:\n%s\n" (str dependency-graph))))
+
+(defn print-document-info [sentences]
+  (doseq [sentence sentences]
+    (print-sentence-info sentence)))
 
 (defn -main
   [& args]
