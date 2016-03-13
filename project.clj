805e349e66de333d0ebc29cdaafec4072d3df14a
Dave Harlan <david.k.harlan@gmail.com>
2016-03-12 19:37:24 -0500

    Added PDFBox dependency and wrote code to grab text from PDFs

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject nlp-demo "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [edu.stanford.nlp/stanford-corenlp "3.6.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.6.0" :classifier "models"]]
+                 [edu.stanford.nlp/stanford-corenlp "3.6.0" :classifier "models"]
+                 [org.apache.pdfbox/pdfbox "1.8.11"]]
   :main ^:skip-aot nlp-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/nlp_demo/core.clj
+++ src/nlp_demo/core.clj
@@ -1,5 +1,24 @@
 (ns nlp-demo.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:import (org.apache.pdfbox.pdfparser PDFParser)
+           (org.apache.pdfbox.pdmodel PDDocument)
+           (org.apache.pdfbox.util PDFTextStripper)))
+
+(defn read-pdf-text [file]
+  (let [parser (PDFParser. (io/input-stream file))]
+    (.parse parser)
+    (with-open [cos-doc (.getDocument parser)
+                pd-doc (PDDocument. cos-doc)]
+      (let [stripper (PDFTextStripper.)]
+        (.getText stripper pd-doc)))))
+
+(defn read-all-pdfs [resource-dir]
+  (map read-pdf-text (-> resource-dir
+                         (io/resource)
+                         (io/file)
+                         (file-seq)
+                         (rest))))
 
 (defn -main
   "I don't do a whole lot ... yet."
